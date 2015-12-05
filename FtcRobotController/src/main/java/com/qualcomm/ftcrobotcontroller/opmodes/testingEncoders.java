4bde3574ee0babd732316b88659c8313ede110ea
FTC-10635 <ftc10635@gmail.com>
2015-12-05 16:14:40 -0600

    Testing the Encoders

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/testingEncoders.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/testingEncoders.java
@@ -8,20 +8,17 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 
 public class testingEncoders extends OpMode {
 
-    DcMotor frontrightMotor;
-    DcMotor frontleftMotor;
-    DcMotor backrightMotor;
-    DcMotor backleftMotor;
-
-
     final static int ENCODER_CPR = 1440;     //Encoder Counts per Revolution
     final static double GEAR_RATIO = 2;      //Gear Ratio
     final static int WHEEL_DIAMETER = 4;     //Diameter of the wheel in inches
     final static int DISTANCE = 24;          //Distance in inches to drive
-
     final static double CIRCUMFERENCE = Math.PI * WHEEL_DIAMETER;
     final static double ROTATIONS = DISTANCE / CIRCUMFERENCE;
     final static double COUNTS = ENCODER_CPR * ROTATIONS * GEAR_RATIO;
+    DcMotor frontrightMotor;
+    DcMotor frontleftMotor;
+    DcMotor backrightMotor;
+    DcMotor backleftMotor;
 
     @Override
     public void init() {
@@ -35,26 +32,22 @@ public class testingEncoders extends OpMode {
 
         backleftMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         backrightMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-        frontleftMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-        frontrightMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+
     }
 
     @Override
     public void start() {
         backleftMotor.setTargetPosition((int) COUNTS);
         backrightMotor.setTargetPosition((int) COUNTS);
-        frontleftMotor.setTargetPosition((int) COUNTS);
-        frontrightMotor.setTargetPosition((int) COUNTS);
+
 
         backleftMotor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
         backrightMotor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
-        frontleftMotor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
-        frontrightMotor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+
 
         backleftMotor.setPower(1.0);
         backrightMotor.setPower(1.0);
-        frontleftMotor.setPower(1.0);
-        frontrightMotor.setPower(1.0);
+
 
 
     }
@@ -64,6 +57,20 @@ public class testingEncoders extends OpMode {
         telemetry.addData("Motor Target", COUNTS);
         telemetry.addData("Left Position", backleftMotor.getCurrentPosition());
         telemetry.addData("Right Position", backrightMotor.getCurrentPosition());
+        if (backleftMotor.getCurrentPosition()<COUNTS) {
+            frontleftMotor.setPower(1.0);
+            frontrightMotor.setPower(1.0);
+        }
+        else {
+            frontleftMotor.setPowerFloat();
+            frontrightMotor.setPowerFloat();
+        }
+
+    }
+    public void resetEncoder() throws InterruptedException {
+        backleftMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        backrightMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+
     }
 }
 
